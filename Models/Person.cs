1fb2dd52563813a90d64f3a5a35a949e5edd831f
LucasPhilipsen <lucaspborgezz@gmail.com>
2026-07-07 17:33:27 -0300

    chore: initial project setup and domain models

---
+++ Models/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace ControleGastos.Api.Models;
+
+public class Person
+{
+    public int Id { get; init; }
+    public string Name { get; set; } = string.Empty;
+    public int Age { get; set; }
+
+    public List<Transaction> Transactions { get; set; } = new();
+}
---
+++ Models/Transaction.cs
@@ -0,0 +1,19 @@
+﻿namespace ControleGastos.Api.Models;
+
+public enum TransactionType
+{
+    Income,
+    Expense
+}
+
+public class Transaction
+{
+    public int Id { get; init; }
+    public string Description { get; set; } = string.Empty;
+    public decimal Amount { get; set; }
+    public TransactionType Type { get; set; }
+
+    public int PersonId { get; set; }
+    
+    public Person? Person { get; set; }
+}
